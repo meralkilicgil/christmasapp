c482907b863c28855a8a4101ea9c1d00852982d3
meralkilicgil <meralkilicgil@gmail.com>
2023-11-30 20:09:33 +0300

    initial commit

---
+++ src/main/java/com/example/christmastapp/ChristmastAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.christmastapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmastAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristmastAppApplication.class, args);
+    }
+
+}
